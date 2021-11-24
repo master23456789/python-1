98d85982148ddbfcdedd9a898e4a79f028ee626a
master23456789 <91898768+master23456789@users.noreply.github.com>
2021-11-24 19:42:46 +0530

    Add files via upload

---
+++ thirdprogram.py
@@ -0,0 +1,7 @@
+#Write a program in python to print the third charecter of a string.
+str = 'HELLO'
+print ("the third charecter of str is",str[2])
+str = 'Umar works hard but he needs to study'
+print ("the fifth charecter of the str is",str[4])
+print ("the fourth charecter of the str is",str[3])
+print ("the tenth charecter of the str is",str[9])
